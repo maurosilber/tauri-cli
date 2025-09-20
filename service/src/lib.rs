4908dfaf18ebd4baadac017bba4d81c1647541fb
Mauro Silberberg <maurosilber@gmail.com>
2025-09-20 19:09:51 -0300

    create a file

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -1,12 +1,19 @@
-use std::io::{self, BufRead};
-use std::process;
+use std::fs::OpenOptions;
+use std::io::Write;
 
-pub fn main() {
-    println!("Write 'exit' to exit.");
-    let stdin = io::stdin();
-    for line in stdin.lock().lines().flatten() {
-        if line.as_str() == "exit" {
-            process::exit(0)
-        }
-    }
+#[tokio::main]
+pub async fn main() {
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("/Users/maurosilber/git/nodevisa/tauri-tray/output.txt")
+        .expect("Unable to open file");
+
+    writeln!(file, "Hello, file!").expect("Unable to write to file");
+    println!("Ctrl+C to exit.");
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to listen for event");
+    println!("Received Ctrl+C. Exit.");
+    writeln!(file, "Goodbye, file!").expect("Unable to write to file");
 }
