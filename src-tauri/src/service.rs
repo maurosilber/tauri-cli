3ed4a00e7df653347dbdb6310717c47e88550721
Mauro Silberberg <maurosilber@gmail.com>
2025-09-20 19:47:44 -0300

    use windows taskkill

--- src-tauri/src/service.rs
+++ src-tauri/src/service.rs
@@ -24,11 +24,25 @@ impl Service {
         Self { _rx: rx, tx }
     }
 
+    #[cfg(target_os = "windows")]
     pub fn shutdown(mut self) {
-        println!("closing service...");
-        match self.tx.write(b"\x03") {
-            Ok(_) => println!("closed service"),
-            Err(e) => eprintln!("could not close the service: {e:?}"),
+        if let Some(pid) = self.tx.pid() {
+            // Send CTRL+C event to the process group
+            let _ = Command::new("taskkill")
+                .args(&["/Pid", &pid.to_string(), "/T", "/F"])
+                .output();
         }
     }
+
+    // #[cfg(not(target_os = "windows"))]
+    // pub fn shutdown(self) {
+    //     println!("Shutting down service...");
+
+    //     use nix::sys::signal::{Signal, kill};
+    //     use nix::unistd::Pid;
+    //     match kill(Pid::from_raw(self.tx.pid() as i32), Signal::SIGINT) {
+    //         Ok(_) => println!("Service shutdown."),
+    //         Err(e) => eprintln!("Error shutting down service: {e:?}"),
+    //     };
+    // }
 }
